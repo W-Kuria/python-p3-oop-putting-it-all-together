059685cc489bbe9b4218a3865d97c7ce8bc4f597
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-28 14:04:47 +0300

    feat: Complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title="", page_count=0):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    # ---- Title ----
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    # ---- Page Count ----
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")   # <── print instead of raise
+
+    # ---- Turn Page ----
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=0):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.condition = "New"
+
+    # ---- Size ----
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")   # <── print instead of raise
+
+    # ---- Repair ----
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
